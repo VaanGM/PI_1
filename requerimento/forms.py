94740abf963ce6377fbc685671b003c845aff912
Vanley Galvão <vanleygalvao@gmail.com>
2021-10-26 12:24:22 -0300

    Adequações aos forms e adição de CSS

--- requerimento/forms.py
+++ requerimento/forms.py
@@ -1,11 +1,17 @@
 from requerimento.models import Municipe, Requisicao
 from django import forms
+from django.forms import Textarea, widgets
+
  
 class MunicipeForm(forms.ModelForm):
     class Meta:
         model = Municipe
         fields = ('nome','email',)
-    
+        widgets = {
+            'nome': forms.TextInput(attrs={'class': 'input_field'}),
+            'email': forms.TextInput(attrs={'class': 'input_field'})
+        }
+
     def __init__(self, *args, **kwargs):
         super(forms.ModelForm, self).__init__(*args, **kwargs)
         self.fields['nome'].label = 'Nome'
@@ -15,6 +21,10 @@ class RequisicaoForm(forms.ModelForm):
     class Meta:
         model = Requisicao
         fields = ('tema','comentario',)
+        widgets = {
+            'tema': forms.TextInput(attrs={'class': 'input_field'}),
+            'comentario': forms.Textarea(attrs={'class': 'input_field'})
+        }
     
     def __init__(self, *args, **kwargs):
         super(forms.ModelForm, self).__init__(*args, **kwargs)
--- requerimento/models.py
+++ requerimento/models.py
@@ -17,7 +17,7 @@ class Requisicao(models.Model):
 
     tema = models.CharField(max_length=200)
     numero = models.IntegerField(default=0)
-    comentario = models.CharField(max_length=500)
+    comentario = models.CharField(max_length=3000)
     data = models.DateTimeField(default=timezone.now, blank=True)
     requerente = models.ForeignKey(Municipe, on_delete = models.CASCADE)
 
